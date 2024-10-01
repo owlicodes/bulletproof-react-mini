5fe76a80785dd5df98b1245be3688dcae49f23c2
owlicodes <owlicodes@gmail.com>
2024-10-01 16:26:29 +0800

    Style the Go to Home link in not found page.

--- src/app/routes/not-found.tsx
+++ src/app/routes/not-found.tsx
@@ -5,7 +5,7 @@ export const NotFoundRoute = () => {
     <div className="mt-52 flex flex-col items-center font-semibold">
       <h1>404 - Not Found</h1>
       <p>Sorry, the page you are looking for does not exist.</p>
-      <Link to="/" replace>
+      <Link to="/" replace className="text-blue-500 underline">
         Go to Home
       </Link>
     </div>
